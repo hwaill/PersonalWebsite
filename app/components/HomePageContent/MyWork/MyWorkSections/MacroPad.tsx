409a0eb337d433d177f7a2834975e31209f982a7
hwaill <hwaill@optonline.net>
2024-05-05 02:41:33 -0600

    begin macropad documentation

--- app/components/HomePageContent/MyWork/MyWorkSections/MacroPad.tsx
+++ app/components/HomePageContent/MyWork/MyWorkSections/MacroPad.tsx
@@ -6,10 +6,10 @@ const MacroPad = () => {
 	return (
 		<div className={style.projectContainer + " neu neuShallow"}>
 			<div className={style.projectImageHolder}>
-				<div className={style.projectImage} id={style.macroPadImage}></div>
+				<div className={style.projectImage} id={style.macropadImage}></div>
 			</div>
 			<div className={style.projectText}>
-			<img src="/img/projects/macropad/logo.svg" id={style.whiteboardLogo} />
+			<img src="/img/projects/macropad/logo.svg" id={style.macropadLogo} />
 				<p>The <em>todos productivity board</em> is a task management tool designed for those who struggle with attention and executive function.</p>
 				<p>At the intersection of analog and digital technologies, <em>todos</em> combines tactile interaction on a familiar interface with the ability to track habit-building progress, trends in mood and much more.</p>
 				<Link href="/projects/macropad/">
