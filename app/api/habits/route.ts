d4eff75c322727c6baff3c27fa29017fd68b4a5e
hwaill <hwaill@optonline.net>
2024-05-17 23:39:01 -0600

    test api

--- app/api/habits/route.ts
+++ app/api/habits/route.ts
@@ -1,6 +1,7 @@
 import { headers } from "next/headers";
+import { NextRequest } from "next/server";
 
-export async function GET(Request) {
+export async function GET(Request: NextRequest) {
 	const headersList = headers();
 	if(headersList.get('key') !== process.env.SILLY_KEY) {
 		return Response.error();
