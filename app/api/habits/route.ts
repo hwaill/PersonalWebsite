82c948f5e0b940f799be4ccec848cc24c5c0bd5f
hwaill <hwaill@optonline.net>
2024-05-17 23:37:53 -0600

    test api

--- app/api/habits/route.ts
+++ app/api/habits/route.ts
@@ -3,7 +3,7 @@ import { headers } from "next/headers";
 export async function GET(Request) {
 	const headersList = headers();
 	if(headersList.get('key') !== process.env.SILLY_KEY) {
-		return null;
+		return Response.error();
 	}
 	return new Response("This is a new API route");
 }
