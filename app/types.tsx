bc24be62e7b9e1547d0a8b525f962c7e94a622f2
hwaill <hwaill@optonline.net>
2026-05-07 22:24:17 -0700

    Undo of shame...

--- app/types.tsx
+++ app/types.tsx
@@ -1,3 +1,102 @@
+export type NavItem = {
+	title: string;
+	url: string;
+	icon?: string;
+	subNav?: boolean;
+	subNavItems?: NavItem[];
+	externalLink?: boolean;
+	ignoreHighlight?: boolean;
+	disabled?: boolean;
+};
+
+export type Project = {
+	heading: string;
+	tags: string[];
+	hook?: string;
+	description: string;
+	imgUrl: string;
+	imgStyle?: string;
+	imgAltText?: string;
+	linkUrl: string;
+	featured: boolean;
+	disabled?: boolean;
+};
+
+export type EmailData = {
+	name: string;
+	email: string;
+	message: string;
+};
+
+export enum RESUME_SIDEBAR_URL_TYPES {
+	EXTERNAL,
+	LOCAL
+}
+
+export type ResumeSidebarItem = {
+	icon: string;
+	text: string;
+	url?: string;
+	urlType?: RESUME_SIDEBAR_URL_TYPES;
+};
+
+export type ResumeExperience = {
+	org: string;
+	position: string;
+	location: string;
+	date: string;
+	descriptions?: string[];
+	logoUrl?: string;
+	logo2Url?: string;
+};
+
+export type ResumeSection = {
+	heading: string;
+	items: ResumeExperience[];
+};
+
+export const enum RESUME_SKILL_TYPE {
+	NUMBER_SCALE,
+	WORD_SCALE,
+	PHRASE,
+	NO_VALUE
+}
+
+export type ResumeSkill = {
+	name: string;
+	description?: string;
+	type: RESUME_SKILL_TYPE;
+	numberValue?: number;
+	wordValue?: string;
+	phraseValue?: string;
+}
+
+export type ResumeSkillCategory = {
+	name: string;
+	subCategories?: ResumeSkillCategory[];
+	skills?: ResumeSkill[];
+	type?: RESUME_SKILL_TYPE;
+	numberValue?: number;
+	wordValue?: string;
+	phraseValue?: string;
+}
+
+export type ResumeSkillsSectionSubSection = {
+	categories?: ResumeSkillCategory[];
+}
+
+export type ResumeSkillsSection = {
+	heading: string;
+	categories?: ResumeSkillCategory[];
+	subSections?: ResumeSkillsSectionSubSection[];
+}
+
+export type BookClubNavItem = {
+	title: string;
+	url: string;
+	icon?: string;
+};
+
 // Book Club data types
 
 export type BCImage = {
